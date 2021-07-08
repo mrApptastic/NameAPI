061ba05d80cfd813ca55ec05ef36a8abbf927bf9
mrApptastic <30831839+mrApptastic@users.noreply.github.com>
2021-07-09 00:46:42 +0200

    Added swagger to names api

--- NameAPI/Startup.cs
+++ NameAPI/Startup.cs
@@ -29,6 +29,9 @@ namespace NameBandit
         {
 
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
             
             services. AddCors(c =>
             {
@@ -53,6 +56,17 @@ namespace NameBandit
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Names API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
