1ca94d36feb31f524f7f44d19876aaba698899df
mrApptastic <henrikbeske@gmail.com>
2022-09-01 15:30:17 +0200

    NamesApp: About view.

---
+++ NameClient/src/components/About.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-sm-12">
+        <h2>Om denne applikation</h2>
+        <p>Denne applikation er</p>
+      </div>
+ 
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'About',
+  components: {},
+  data: function () {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
